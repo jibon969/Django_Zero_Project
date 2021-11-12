bd1bc2dcbdd7050300517f81e75eca76a466bacc
= <=>
2021-11-12 23:29:52 +0600

    register product & brand model

--- csv_file/admin.py
+++ csv_file/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Brand, Product
 
-# Register your models here.
+
+class BrandAdmin(admin.ModelAdmin):
+
+    class Meta:
+        model = Brand
+        list_display = ['title', 'slug', 'image', 'active', 'timestamp']
+        search_fields = ['title']
+
+
+admin.site.register(Brand, BrandAdmin)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    class Meta:
+        model = Product
+        list_display = ['title', 'brand', 'timestamp']
+        search_fields = ['title']
+
+
+admin.site.register(Product, ProductAdmin)
