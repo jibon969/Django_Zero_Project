3efccd42348bca5984f5583676adb752b45737b1
= <=>
2021-11-15 23:53:03 +0600

    added download csv file

--- csv_file/urls.py
+++ csv_file/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from csv_file.views import (
-    product_list
+    product_list,
+    download_product_csv
+
 )
 
 urlpatterns = [
     path('product-list/', product_list, name="product-list"),
+    path('download-product-csv/', download_product_csv, name="download-product-csv"),
 ]
